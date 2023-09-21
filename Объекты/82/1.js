caca4db172a22e56e280a3e8deec57d74cc4664d
k0sen4n47 <zaharka25@gmail.com>
2023-09-21 09:13:56 +0300

    Tyulkin 21-09-23

---
+++ Объекты/82/1.js
@@ -0,0 +1,5 @@
+let obj = {'name': 'Захахахар', 
+'surname': 'Тюлькин',
+'patronymic': 'Олегович'};
+for(let key in obj){
+console.log(obj['surname'] + ' ' + obj['name'] + ' ' + obj['patronymic']);}
